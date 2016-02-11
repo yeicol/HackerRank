f9a8d9bb84c833c506a4d31d66c639cefe8b7df3
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-02-10 21:52:24 -0500

    remove code unnecessary

--- java/JavaDatatypes.java
+++ java/JavaDatatypes.java
@@ -21,22 +21,22 @@ public class JavaDatatypes {
                 long n = Long.parseLong(s);
                 System.out.println(n + " can be fitted in:");
                 if (n >= -128 && n <= 127) {
-                    System.out.println("* byte");
-                    System.out.println("* short");
-                    System.out.println("* int");
-                    System.out.println("* long");
+                    out.println("* byte");
+                    out.println("* short");
+                    out.println("* int");
+                    out.println("* long");
                 } else if (n >= -32768 && n <= 32767) {
-                    System.out.println("* short");
-                    System.out.println("* int");
-                    System.out.println("* long");
+                    out.println("* short");
+                    out.println("* int");
+                    out.println("* long");
                 } else if (n >= -2147483648 && n <= 2147483647) {
-                    System.out.println("* int");
-                    System.out.println("* long");
+                    out.println("* int");
+                    out.println("* long");
                 } else {
-                    System.out.println("* long");
+                    out.println("* long");
                 }
             } catch (NumberFormatException e) {
-                System.out.println(s + " can't be fitted anywhere.");
+                out.println(s + " can't be fitted anywhere.");
             }
 
         }
