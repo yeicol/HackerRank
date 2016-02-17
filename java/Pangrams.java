5d212ba85f7371d209a9a53f4a1d165e2ae6df15
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-02-17 07:55:37 -0500

    Solution to challenge: Pangrams

---
+++ java/Pangrams.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+public class Pangrams {
+
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter out = new PrintWriter(System.out);
+	static final char[] a = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+	public static void main(String[] args) throws IOException {
+		String s = in.readLine();
+		boolean f = true;
+		for (int i = 0; i < a.length; i++) {
+			String l = String.valueOf(a[i]);
+			if (!s.toLowerCase().contains(l)) {
+				f = false;
+				break;
+			}
+		}
+		out.println(f ? "pangram" : "not pangram");
+		out.close();
+	}
+}
