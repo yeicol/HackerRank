8fd4f925da17c1aff92fb2bd88056e2475ad5867
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-02-25 17:43:28 -0500

    refactored code

--- java/FixTheCycles.java
+++ java/FixTheCycles.java
@@ -1,55 +1,33 @@
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import static java.lang.Math.abs;
+import static jdk.nashorn.internal.objects.NativeMath.min;
 
 /**
  * See <a href="https://www.hackerrank.com/contests/w19/challenges/fix-the-cycles">Fix the Cycles</a>
  * @author Brian Yeicol Restrepo Tangarife
  */
-public class FixTheCycles {
+ public class FixTheCycles {
 
-	public static Scanner sc = new Scanner(System.in);
-	public static int a = scan();
-	public static int b = scan();
-	public static int c = scan();
-	public static int d = scan();
-	public static int e = scan();
-	public static int f = scan();
+ 	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+ 	static PrintWriter out = new PrintWriter(System.out);
 
-	public static void main(String[] args) {
+ 	public static void main(String[] args) throws IOException {
 
-		if ((a + b + f) < 0 && (a + e + d) > 0 && (f < 0 && e < 0 || b < 0 && d < 0)) {
-			System.out.println(-1);
-		} else {
-			int p = 0;
-			while (true) {
-
-				if ((a + p + b + f) >= 0 && (a + p + b + c + d) >= 0 && (a + p + e + d) >= 0) {
-					break;
-				}
-				if ((a + p + b + f) >= 0 && (a + p + b + c + d) >= 0 && (a + e + d) >= 0) {
-					break;
-				}
-				if ((a + b + f) >= 0 && (a +p + b + c + d) >= 0 && (a + e + d) >= 0) {
-					break;
-				}
-				if ((a + b + f) >= 0 && (a +p + b + c + d) >= 0 && (a + e +p + d) >= 0) {
-					break;
-				}
-				if ((a + b + f) >= 0 && (a + b + c + d) >= 0 && (a + e +p + d) >= 0) {
-					break;
-				}
-				if ((a + p + b + f) >= 0 && (a + b + c + d) >= 0 && (a + e + d) >= 0) {
-					break;
-				}
-				p++;
-			}
-			System.out.println(p);
-		}
-
-	}
-
-	private static int scan() {
-		return sc.nextInt();
-	}
-
-}
+ 		String[] P = in.readLine().split(" ");
+ 		int a = Integer.parseInt(P[0]);
+ 		int b = Integer.parseInt(P[1]);
+ 		int c = Integer.parseInt(P[2]);
+ 		int d = Integer.parseInt(P[3]);
+ 		int e = Integer.parseInt(P[4]);
+ 		int f = Integer.parseInt(P[5]);
+ 		int C1 = a + b + c + d;
+ 		int C2 = a + b + f;
+ 		int C3 = a + d + e;
+ 		out.println(abs((int) min(0, C1, C2, C3)));
+ 		out.close();
+ 	}
+ }
