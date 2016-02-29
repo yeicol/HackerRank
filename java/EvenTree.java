27eec7963211375448664fc34c193f6688aa0d95
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-02-28 21:52:59 -0500

    Refactored code

--- java/EvenTree.java
+++ java/EvenTree.java
@@ -1,56 +1,59 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
-public class Solution {
+/**
+ * See <a href="https://www.hackerrank.com/challenges/even-tree">Even Tree</a>
+ *
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+public class EvenTree {
 
-        /**
-         * @param args
-         */
-       
-        public static void main(String[] args) {
-                // TODO Auto-generated method stub
-               
-                int tree[];
-                int count[];
-               
-                Scanner scan = new Scanner(System.in);
-               
-                int N = scan.nextInt(); //points
-                int M = scan.nextInt();
-               
-                tree = new int[N];
-                count = new int[N];
-                Arrays.fill(count, 1);
-               
-                for(int i=0;i < M; i++)
-                {
-                        int u1 = scan.nextInt();
-                    int v1 = scan.nextInt();
-                   
-                    tree[u1-1] = v1;
-                   
-                    count[v1-1] += count[u1-1];
-                   
-                    int root = tree[v1-1];
-                   
-                    while(root!=0)
-                    {
-                        count[root-1] += count[u1-1];
-                        root = tree[root-1];
-                    }
-                }
-                   
-            int counter = -1;
-                for(int i=0;i < M; i++)
-                {
-                        if(count[i]%2==0)
-                        {
-                                counter++;
-                        }
-                   
-                }
-                System.out.println(counter);
-               
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws IOException {
+
+        int t[];
+        int q[];
+        String[] P = in.readLine().split(" ");
+        int N = Integer.parseInt(P[0]);
+        int M = Integer.parseInt(P[1]);
+
+        t = new int[N];
+        q = new int[N];
+        Arrays.fill(q, 1);
+
+        for (int i = 0; i < M; i++) {
+            P = in.readLine().split(" ");
+            int u = Integer.parseInt(P[0]);
+            int v = Integer.parseInt(P[1]);
+
+            t[u - 1] = v;
+
+            q[v - 1] += q[u - 1];
+
+            int root = t[v - 1];
+
+            while (root != 0) {
+                q[root - 1] += q[u - 1];
+                root = t[root - 1];
+            }
         }
+
+        int c = -1;
+        for (int i = 0; i < M; i++) {
+            if (q[i] % 2 == 0) {
+                c++;
+            }
+
+        }
+        out.println(c);
+        out.close();
+
+    }
 
 }
