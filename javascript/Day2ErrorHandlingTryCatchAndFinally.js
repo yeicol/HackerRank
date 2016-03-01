ce8eebd4685bfb519f030bc649b598538d279cc8
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-01 15:49:04 -0500

    Solution to challenge: Day 2 Error Handling - Try, Catch, and Finally

---
+++ javascript/Day2ErrorHandlingTryCatchAndFinally.js
@@ -0,0 +1,31 @@
+/**
+ * See <a href="https://www.hackerrank.com/contests/javascript-week2/challenges/js-try-catch">Day 2: Error Handling - Try, Catch, and Finally</a>
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+function processData(my_string) {
+    try{
+        my_string = my_string.split('').reverse().join('')
+        console.log(`Reversed string is : ${my_string}`);
+
+    } catch (err){
+
+       console.log(`Error : ${err.message}`);
+
+    } finally {
+
+        console.log(`Type of my_string is : ${typeof my_string}`)
+
+    }
+}
+
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(eval(_input));
+});
