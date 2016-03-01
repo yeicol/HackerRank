6ba7fc9b05bf8a98dc55bac33858c1da422c5ce3
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-01 15:53:09 -0500

    Solution to challenge: Day 2 Error Handling - Throw

---
+++ javascript/Day2ErrorHandlingThrow.js
@@ -0,0 +1,33 @@
+/**
+ * See <a href="https://www.hackerrank.com/contests/javascript-week2/challenges/js-throw">Day 2: Error Handling - Throw</a>
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+function processData(my_height) {
+    try {
+        if(isNaN(my_height)) {
+            throw new Error("notANumberError");
+        }
+        if(my_height > 10) {
+            throw new Error("hugeHeightError");
+        }
+        if(my_height < 4) {
+            throw new Error("tinyHeightError");
+        }
+        console.log(my_height);
+
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
