e82ba53756a6514f5d92e50f882aa24b362b19f4
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-05 20:08:33 -0500

    add challenge source

--- javascript/Day5CreateAButton/js/button.js
+++ javascript/Day5CreateAButton/js/button.js
@@ -1,3 +1,9 @@
+
+/**
+ * See <a href="https://www.hackerrank.com/contests/javascript-week2/challenges/create-a-button">Day 5: Create a Button</a>
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+ 
 var $btn = document.getElementById("btn"); 
 $btn.addEventListener("click", function(){
     var value = parseInt(this.innerHTML, 10) + 1;
