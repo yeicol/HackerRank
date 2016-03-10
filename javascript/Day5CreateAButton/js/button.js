607ac5b180587dfe348e0b5c4dceda143cee0894
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-10 17:59:10 -0500

    Update button.js

--- javascript/Day5CreateAButton/js/button.js
+++ javascript/Day5CreateAButton/js/button.js
@@ -3,7 +3,6 @@
  * See <a href="https://www.hackerrank.com/contests/javascript-week2/challenges/create-a-button">Day 5: Create a Button</a>
  * @author Brian Yeicol Restrepo Tangarife
  */
- 
 var $btn = document.getElementById("btn"); 
 $btn.addEventListener("click", function(){
     var value = parseInt(this.innerHTML, 10) + 1;
