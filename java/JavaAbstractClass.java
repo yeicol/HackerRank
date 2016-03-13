1bbd8c049238289471259cbfc2791b2918ad609f
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-12 21:07:51 -0500

    Update challenge url

--- java/JavaAbstractClass.java
+++ java/JavaAbstractClass.java
@@ -1,4 +1,22 @@
-import java.util.*;
+import java.util
+
+/**
+ * See <a href="https://www.hackerrank.com/challenges/java-abstract-class">Java Abstract Class</a>
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+public class JavaAbstractClass
+{
+
+   public static void main(String []args)
+   {
+      Scanner sc=new Scanner(System.in);
+      String title=sc.nextLine();
+      MyBook new_novel=new MyBook();
+      new_novel.setTitle(title);
+      System.out.println("The title is: "+new_novel.getTitle());
+
+   }
+}
 
 abstract class Book
 {
@@ -15,18 +33,4 @@ class MyBook extends Book{
     void setTitle(String s){
         this.title = s;
     }
-}
-
-public class JavaAbstractClass
-{
-
-   public static void main(String []args)
-   {
-      Scanner sc=new Scanner(System.in);
-      String title=sc.nextLine();
-      MyBook new_novel=new MyBook();
-      new_novel.setTitle(title);
-      System.out.println("The title is: "+new_novel.getTitle());
-
-   }
 }
