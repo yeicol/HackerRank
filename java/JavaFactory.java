a21a0de99f513644a76fdf7f38b404656d710f03
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-12 21:10:53 -0500

    added challenge url

--- java/JavaFactory.java
+++ java/JavaFactory.java
@@ -1,5 +1,35 @@
 import java.util.*;
 import java.security.*;
+
+/**
+ * See <a href="https://www.hackerrank.com/challenges/java-factory">Java Factory</a>
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+public class Solution {
+
+ public static void main(String args[]){
+		Do_Not_Terminate.forbidExit();
+
+	try{
+
+		Scanner sc=new Scanner(System.in);
+		//creating the factory
+		FoodFactory foodFactory = new FoodFactory();
+
+		//factory instantiates an object
+		Food food = foodFactory.getFood(sc.nextLine());
+
+
+		System.out.println("The factory returned "+food.getClass());
+		System.out.println(food.getType());
+	}
+	catch (Do_Not_Terminate.ExitTrappedException e) {
+		System.out.println("Unsuccessful Termination!!");
+	}
+ }
+
+}
+
 interface Food {
 	 public String getType();
 	}
@@ -17,46 +47,23 @@ interface Food {
 	}
 	class FoodFactory {
 		public Food getFood(String order) {
-		    if(order.equals("cake")){ 
-            return new Cake(); 
-        } else{ 
-            return new Pizza(); 
+		    if(order.equals("cake")){
+            return new Cake();
+        } else{
+            return new Pizza();
         }
   //End of getFood method
 
 	}//End of factory class
 
-	public class Solution {
 
-	 public static void main(String args[]){
-			Do_Not_Terminate.forbidExit();
-
-		try{
-
-			Scanner sc=new Scanner(System.in);
-			//creating the factory
-			FoodFactory foodFactory = new FoodFactory();
-	
-			//factory instantiates an object
-			Food food = foodFactory.getFood(sc.nextLine());
-	
-			
-			System.out.println("The factory returned "+food.getClass());
-			System.out.println(food.getType());
-		}
-		catch (Do_Not_Terminate.ExitTrappedException e) {
-			System.out.println("Unsuccessful Termination!!");
-		}
-	 }
-
-	}
 	class Do_Not_Terminate {
-		 
+
 	    public static class ExitTrappedException extends SecurityException {
 
 			private static final long serialVersionUID = 1L;
 	    }
-	 
+
 	    public static void forbidExit() {
 	        final SecurityManager securityManager = new SecurityManager() {
 	            @Override
@@ -68,5 +75,4 @@ interface Food {
 	        };
 	        System.setSecurityManager(securityManager);
 	    }
-	}	
-		
+	}
