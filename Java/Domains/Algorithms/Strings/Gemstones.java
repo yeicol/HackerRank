c26b406c4e911d1052cfe758a595ea8fdb7a0c05
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-13 11:26:15 -0500

    Create Gemstones.java

---
+++ Java/Domains/Algorithms/Strings/Gemstones.java
@@ -0,0 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * See <a href="https://www.hackerrank.com/challenges/gem-stones/submissions">Gemstones</a>
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+public class Gemstones {
+
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(in.readLine());
+        List<String> rocks = new ArrayList<>();
+        while (N-- > 0) {
+            rocks.add(in.readLine());
+        }
+
+        List<String> alphabet = Arrays.asList("abcdefghijklmnopqrstuvwxyz".split(""));
+
+        Stream<String> gemstones = alphabet.stream().filter(
+                letter -> rocks.stream().filter(
+                        rock -> rock.contains(letter)).count() == rocks.size()
+        );
+
+        out.print(gemstones.toArray().length);
+        out.close();
+    }
+}
