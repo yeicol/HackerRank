542372eae3f766291b4a6d6975ee325e4d1ed2dc
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-25 08:09:33 -0500

    Solution to the challenge: Service Lane

--- Java/Domains/Algorithms/Implementation/ServiceLane.java
+++ Java/Domains/Algorithms/Implementation/ServiceLane.java
@@ -6,7 +6,7 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 
 /**
- *
+ * See <a href="https://www.hackerrank.com/challenges/service-lane">Srvice Lane</a>
  * @author Brian Yeicol Restrepo Tangarife
  */
 public class ServiceLane {
@@ -16,7 +16,22 @@ public class ServiceLane {
 
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(in.readLine().split(" ")[1]);
-        int[]N = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] N = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int i, j, min;
+        String[] P;
+
+        while (T-- > 0) {
+            P = in.readLine().split(" ");
+            i = Integer.parseInt(P[0]);
+            j = Integer.parseInt(P[1]);
+            min = 3;
+
+            while (min > 1 && i <= j) {
+                min = (N[i] < min) ? N[i] : min;
+                i++;
+            }
+            out.println(min);
+        }
         out.close();
     }
 }
