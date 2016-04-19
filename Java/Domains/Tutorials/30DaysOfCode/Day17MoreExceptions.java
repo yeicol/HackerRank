700dd578d5e81c1e496bd9bb86863f09bdc527e1
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-04-19 13:43:42 -0500

    Create Day17MoreExceptions.java

---
+++ Java/Domains/Tutorials/30DaysOfCode/Day17MoreExceptions.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * See <a href="https://www.hackerrank.com/challenges/30-more-exceptions">Day 17: More Exceptions</a>
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+public class Day17MoreExceptions {
+
+    public static void main(String []argh)
+    {
+        Scanner in = new Scanner(System.in);
+        int T=in.nextInt();
+        while(T-->0)
+        {
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try
+            {
+                int ans=myCalculator.power(n,p);
+                System.out.println(ans);
+                
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+}
+
+class Calculator {
+    
+    public int power(int n, int p) throws Exception{
+        if(n < 0 || p < 0) {
+            throw new Exception("n and p should be non-negative");
+        }
+        else {
+            return (int) Math.pow(n, p);
+        }
+    }
+}
