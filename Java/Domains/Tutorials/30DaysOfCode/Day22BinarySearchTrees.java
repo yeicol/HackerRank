694693f45da15d9b013c7d0126f64400f64ca2e6
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-04-24 21:14:07 -0500

    create Day22BinarySearchTrees.java

---
+++ Java/Domains/Tutorials/30DaysOfCode/Day22BinarySearchTrees.java
@@ -0,0 +1,59 @@
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * See<a href="https://www.hackerrank.com/challenges/30-binary-search-trees">Day
+ * 22: Binary Search Trees</a>
+ *
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+class Day22BinarySearchTrees {
+
+	public static int getHeight(Node root) {
+		if (root == null) {
+			return -1;
+		}
+		return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+	}
+
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		Node root = null;
+		while (T-- > 0) {
+			int data = sc.nextInt();
+			root = insert(root, data);
+		}
+		int height = getHeight(root);
+		System.out.println(height);
+	}
+}
+
+class Node {
+
+	Node left;
+	Node right;
+	int data;
+
+	Node(int data) {
+		this.data = data;
+		left = right = null;
+	}
+}
