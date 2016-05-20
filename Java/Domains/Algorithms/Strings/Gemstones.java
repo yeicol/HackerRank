be2e321ccc395a96d38eebbe2e4242b75d4c72a3
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-05-20 09:00:27 -0500

    Update Gemstones.java

--- Java/Domains/Algorithms/Strings/Gemstones.java
+++ Java/Domains/Algorithms/Strings/Gemstones.java
@@ -10,28 +10,33 @@ import java.util.stream.Stream;
 
 /**
  * See <a href="https://www.hackerrank.com/challenges/gem-stones">Gemstones</a>
+ *
  * @author Brian Yeicol Restrepo Tangarife
  */
 public class Gemstones {
 
-    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    static PrintWriter out = new PrintWriter(System.out);
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter out = new PrintWriter(System.out);
 
-    public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(in.readLine());
-        List<String> rocks = new ArrayList<>();
-        while (N-- > 0) {
-            rocks.add(in.readLine());
-        }
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(in.readLine());
+		List<String> rocks = new ArrayList<>();
+		while (N-- > 0) {
+			rocks.add(in.readLine());
+		}
 
-        List<String> alphabet = Arrays.asList("abcdefghijklmnopqrstuvwxyz".split(""));
+		List<String> alphabet = Arrays.asList("abcdefghijklmnopqrstuvwxyz".split(""));
 
-        Stream<String> gemstones = alphabet.stream().filter(
-                letter -> rocks.stream().filter(
-                        rock -> rock.contains(letter)).count() == rocks.size()
-        );
+		Stream<String> gemstones = alphabet.stream()
+				.filter(
+						letter -> rocks.stream()
+						.filter(
+								rock -> rock.contains(letter)
+						)
+						.count() == rocks.size()
+				);
 
-        out.print(gemstones.toArray().length);
-        out.close();
-    }
+		out.print(gemstones.toArray().length);
+		out.close();
+	}
 }
